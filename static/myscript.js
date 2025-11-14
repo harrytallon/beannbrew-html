8ff6f22d7825749ce2a8b309b54ab552e86e69fd
lukeh711 <lukehatherly08@gmail.com>
2025-11-14 12:57:51 +0000

    Update myscript.js

--- static/myscript.js
+++ static/myscript.js
@@ -1,1 +1,13 @@
-//pass
+$(document).ready(function() {//runs when the doccument loads
+    $.get("http://10.0.0.1/customers", function(data) {
+        console.log("data from API:", data);
+
+        let output="";
+        data.forEach(customer => {
+            output += `<li>${customer.id}: ${customer.name}</li>`
+
+        });
+
+        $("#db-output").html(output)
+    })
+});
