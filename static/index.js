141ded04dc7ed329b2824d2f69321f0b9775fa4e
lukeh711 <lukehatherly08@gmail.com>
2025-11-19 14:31:35 +0000

    Update index.js

--- static/index.js
+++ static/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         // Example: show it on the page
         let output = "";
         data.forEach(customer => {
-            output += `<li>${customerID}: ${customer.name}</li>`;
+            output += `<li>${customer.customerID}: ${customer.name}</li>`;
         });
 
         $("#db-output").html(output);
