36805de992d1ab7b998e8e7733688aaf7b45be52
lukeh711 <lukehatherly08@gmail.com>
2025-12-03 12:59:48 +0000

    create unflaskified

---
+++ Unflaskified/static/index.js
@@ -0,0 +1,21 @@
+console.log("index.js loaded!");
+
+$(document).ready(function() {
+
+    // Make a GET request to your Flask API
+    $.get("http://10.0.0.1/customers", function(data) {
+        console.log("API data:", data);
+
+        // Example: show it on the page
+        let output = "";
+        data.forEach(customer => {
+            output += `<li>${customer.customerID}: ${customer.name}</li>`;
+        });
+
+        $("#db-output").html(output);
+    })
+    .fail(function() {
+        console.error("Failed to fetch data from API.");
+    });
+
+});
